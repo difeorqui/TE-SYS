3ca2dff1aecfc956bec18fb0550a38c40dc170db
difeorqui <dfoq76@hotmail.com>
2021-12-01 19:15:52 -0500

    Dejar el front para heroku

--- API/src/mailer.js
+++
@@ -1,7 +0,0 @@
-const nodemailer = require("nodemailer");
-const path = require("path");
-const config = require(path.resolve("config.json"));
-
-let transporter = nodemailer.createTransport(config.mailer);
-
-module.exports = transporter;
